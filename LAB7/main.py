2b6cda4d9cfc19cf0e65d5fea3c60a64c1b71915
pay-me-for-the-lab <91729884+pay-me-for-the-lab@users.noreply.github.com>
2023-05-16 17:42:33 +0300

    Update main.py

--- LAB7/main.py
+++ LAB7/main.py
@@ -1,92 +1,92 @@
 import random
 
-numWords, wordLen = 30, 8
+num_words, word_len = 11, 8
 
 
-def words_array():
-    words = [[0] * wordLen for _ in range(numWords)]
-    for i in range(numWords):
-        for j in range(wordLen):
+def words_wordsay():
+    words = [[0] * word_len for _ in range(num_words)]
+    for i in range(num_words):
+        for j in range(word_len):
             words[i][j] = random.randint(0, 1)
     return words
 
 
-def find(arr, attribute, g, l, j):
-    g1 = calc_g(g, attribute[j], l, arr[j])
-    l1 = calc_l(g, attribute[j], l, arr[j])
+def find(words, choosen, g, l, j):
+    g1 = calculate_g(g, choosen[j], l, words[j])
+    l1 = calculate_l(g, choosen[j], l, words[j])
     if j == 0:
         return g1, l1
     else:
-        return find(arr, attribute, g1, l1, j - 1)
+        return find(words, choosen, g1, l1, j - 1)
 
 
-def calc_g(g, a, l, s):
+def calculate_g(g, a, l, s):
     if a == 0 and l == 0 and s == 1:
         return 1
     else:
         return g
 
 
-def calc_l(g, a, l, s):
+def calculate_l(g, a, l, s):
     if a == 1 and s == 0 and g == 0:
         return 1
     else:
         return l
 
 
-def find_less(arr, attribute):
+def find_less(words, choosen):
     g, l = 0, 0
-    arr_less = []
-    for i in range(len(arr)):
-        g, l = find(arr[i], attribute, g, l, wordLen - 1)
+    words_less = []
+    for i in range(len(words)):
+        g, l = find(words[i], choosen, g, l, word_len - 1)
         if g == 0 and l == 1:
-            arr_less.append(arr[i])
+            words_less.append(words[i])
         g, l = 0, 0
-    if len(arr_less) == 0:
-        return [0] * wordLen
-    curr_biggest = arr_less[0]
-    for i in range(len(arr_less)):
+    if len(words_less) == 0:
+        return [0] * word_len
+    curr_biggest = words_less[0]
+    for i in range(len(words_less)):
         g, l = 0, 0
-        g, l = find(arr_less[i], curr_biggest, g, l, wordLen - 1)
+        g, l = find(words_less[i], curr_biggest, g, l, word_len - 1)
         if g == 1 and l == 0:
-            curr_biggest = arr_less[i]
+            curr_biggest = words_less[i]
     return curr_biggest
 
 
-def find_more(arr, attribute):
+def find_more(words, choosen):
     g, l = 0, 0
-    arr_less = []
-    for i in range(len(arr)):
-        g, l = find(arr[i], attribute, g, l, wordLen - 1)
+    words_less = []
+    for i in range(len(words)):
+        g, l = find(words[i], choosen, g, l, word_len - 1)
         if g == 1 and l == 0:
-            arr_less.append(arr[i])
+            words_less.append(words[i])
         g, l = 0, 0
-    if len(arr_less) == 0:
-        return [0] * wordLen
-    curr_biggest = arr_less[0]
-    for i in range(len(arr_less)):
+    if len(words_less) == 0:
+        return [0] * word_len
+    curr_biggest = words_less[0]
+    for i in range(len(words_less)):
         g, l = 0, 0
-        g, l = find(arr_less[i], curr_biggest, g, l, wordLen - 1)
+        g, l = find(words_less[i], curr_biggest, g, l, word_len - 1)
         if g == 0 and l == 1:
-            curr_biggest = arr_less[i]
+            curr_biggest = words_less[i]
     return curr_biggest
 
 
-def find_interval(arr, top, bottom):
+def find_interval(words, top, bottom):
     interval = []
-    out_of_interval = find_out_of_interval(arr, top, bottom)
-    for i in range(len(arr)):
+    out_of_interval = find_out_of_interval(words, top, bottom)
+    for i in range(len(words)):
         if not out_of_interval[i]:
-            interval.append(arr[i])
+            interval.append(words[i])
     return interval
 
 
-def find_out_of_interval(arr, top, bottom):
+def find_out_of_interval(words, top, bottom):
     g_top, l_top, g_bottom, l_bottom = 0, 0, 0, 0
-    out_of_interval = [False] * len(arr)
-    for i in range(len(arr)):
-        g_bottom, l_bottom = find(arr[i], top, g_top, l_top, wordLen - 1)
-        g_top, l_top = find(arr[i], bottom, g_top, l_top, wordLen - 1)
+    out_of_interval = [False] * len(words)
+    for i in range(len(words)):
+        g_bottom, l_bottom = find(words[i], top, g_top, l_top, word_len - 1)
+        g_top, l_top = find(words[i], bottom, g_top, l_top, word_len - 1)
         if g_top == 0 and l_top == 1:
             out_of_interval[i] = True
         if g_bottom == 1 and l_bottom == 0:
@@ -96,19 +96,20 @@ def find_out_of_interval(arr, top, bottom):
 
 
 def main():
-    arr = words_array()
-    for i in range(len(arr)):
-        print(i, ": ", arr[i])
-    find = arr[2]
-    top = arr[5]
-    bottom = arr[2]
-    print("\nNearest-bottom:")
-    print("Find ", find)
-    print("Less ", find_less(arr, find))
-    print("More ", find_more(arr, find))
-    print("\nInterval:\nTop ", top, "  Bottom ", bottom)
-    print("\nFounded interval:")
-    interval = find_interval(arr, top, bottom)
+    words = words_wordsay()
+    print("\nСловарь:")
+    for i in range(len(words)):
+        print(words[i])
+    choosen = words[5]
+    start = words[2]
+    end = words[9]
+    print("\nПоиск ближайшего сверху и снизу значения:")
+    print("Выбранное слово", choosen)
+    print("Снизу", find_less(words, choosen))
+    print("Сверху", find_more(words, choosen))
+    print("\nИнтервал:\nОт", start, "\nДо", end)
+    print("\nНайденные слова на интервале:")
+    interval = find_interval(words, start, end)
     for i in range(len(interval)):
         print(interval[i])
 
